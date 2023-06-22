7658b7fed01246dcfa1ba6432ea573a2735ff8a8
Morterolath <ayberkk0705@gmail.com>
2023-06-22 17:27:37 +0300

    new option for selecting all the sectors is added to the TargetSetter

--- Assets/Scripts/Path Finding/Controllers/TargetSetter.cs
+++ Assets/Scripts/Path Finding/Controllers/TargetSetter.cs
@@ -5,11 +5,27 @@ using static UnityEngine.GraphicsBuffer;
 public class TargetSetter : MonoBehaviour
 {
     NativeList<Vector3> _sources;
+    NativeList<Vector3> _sourcesForEachSector;
     Vector3 _target;
     [SerializeField] PathfindingManager _pathfindingManager;
+    [SerializeField] bool _placeSourceForEachSector;
     private void Start()
     {
         _sources = new NativeList<Vector3>(Allocator.Persistent);
+        _sourcesForEachSector = new NativeList<Vector3>(Allocator.Persistent);
+        NativeArray<Vector3> tilePositions = _pathfindingManager.TilePositions;
+        float tileSize = _pathfindingManager.TileSize;
+        float sectorColAmount = _pathfindingManager.SectorTileAmount;
+        int sectorMatrixCol = _pathfindingManager.SectorMatrixColAmount;
+        int sectorMatrixRow = _pathfindingManager.SectorMatrixRowAmount;
+        for(int i = 0; i < sectorMatrixRow; i++)
+        {
+            for(int j = 0; j < sectorMatrixCol; j++)
+            {
+                Vector3 pos = new Vector3(j * sectorColAmount * tileSize + tileSize * 2, 0f, i * sectorColAmount * tileSize + tileSize * 2);
+                _sourcesForEachSector.Add(pos);
+            }
+        }
     }
     private void Update()
     {
@@ -32,7 +48,16 @@ public class TargetSetter : MonoBehaviour
             {
                 Vector3 hitPos = hit.point;
                 _target = hitPos;
-                NativeArray<Vector3> sorucesCopy = new NativeArray<Vector3>(_sources, Allocator.Persistent);
+                
+                NativeArray<Vector3> sorucesCopy ;
+                if (_placeSourceForEachSector)
+                {
+                    sorucesCopy = new NativeArray<Vector3>(_sourcesForEachSector, Allocator.Persistent);
+                }
+                else
+                {
+                    sorucesCopy = new NativeArray<Vector3>(_sources, Allocator.Persistent);
+                }
                 _pathfindingManager.SetDestination(sorucesCopy, _target);
             }
         }
@@ -41,7 +66,6 @@ public class TargetSetter : MonoBehaviour
             _sources.Clear();
         }
     }
-
     private void OnDrawGizmos()
     {
         if (!_sources.IsCreated) { return; } 
