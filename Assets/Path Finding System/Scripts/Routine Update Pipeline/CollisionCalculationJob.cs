e332039e9b23a92381cca24960379b15d0c9f457
Morterolath <ayberkk0705@gmail.com>
2023-07-30 12:13:52 +0300

    a bug is fixed in collision resolution

--- Assets/Path Finding System/Scripts/Routine Update Pipeline/CollisionCalculationJob.cs
+++ Assets/Path Finding System/Scripts/Routine Update Pipeline/CollisionCalculationJob.cs
@@ -4,6 +4,7 @@ using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEditor.AssetImporters;
 using UnityEngine;
 using UnityEngine.Jobs;
 using UnityEngine.Rendering;
@@ -31,22 +32,17 @@ public struct CollisionCalculationJob : IJobParallelForTransform
         int2 agentIndex = new int2((int)math.floor(agentPos2d.x / TileSize), (int)math.floor(agentPos2d.y / TileSize));
         int agentIndex1d = agentIndex.y * FieldColAmount + agentIndex.x;
         NativeList<int> wallIndiciesAround = GetWallObjectsAround(agentIndex1d);
-        NativeList<Edge> collidingEdges = new NativeList<Edge>(Allocator.Temp);
-        NativeList<float2> seperationForces = new NativeList<float2>(Allocator.Temp);
-        for (int j = 0; j < wallIndiciesAround.Length; j++)
+        if(wallIndiciesAround.Length > 0)
         {
-            WallObject wall = WallObjectList[wallIndiciesAround[j]];
-            collidingEdges.Clear();
-            CheckCollisionsForce(wall, agentPos2d, AgentMovementData[index].Radius, collidingEdges, seperationForces);
-        }
-        float2 sum = 0;
-        if (seperationForces.Length != 0)
-        {
-            for(int i = 0; i < seperationForces.Length; i++)
+            Collision collisionOutpu;
+            if (GetCollision(WallObjectList[wallIndiciesAround[0]], agentPos2d, AgentMovementData[index].Radius, out collisionOutpu))
             {
-                sum += seperationForces[i];
+                float2 seperationForce = SolveCollision(collisionOutpu, agentPos2d, AgentMovementData[index].Radius);
+                transform.position = transform.position + new Vector3(seperationForce.x, 0f, seperationForce.y);
+                agentPos = transform.position;
+                agentPos2d.x = agentPos.x;
+                agentPos2d.y = agentPos.z;
             }
-            transform.position = transform.position + new Vector3(sum.x, 0f, sum.y);
         }
         
         
@@ -57,24 +53,16 @@ public struct CollisionCalculationJob : IJobParallelForTransform
         int2 destIndex = new int2((int)math.floor(dest2d.x / TileSize), (int)math.floor(dest2d.y / TileSize));
         int destIndex1d = destIndex.y * FieldColAmount + destIndex.x;
         NativeList<int> wallIndiciesAroundDest = GetWallObjectsAround(destIndex1d);
-        NativeList<Edge> collidingEdgesDest = new NativeList<Edge>(Allocator.Temp);
-        NativeList<float2> seperationForcesDest = new NativeList<float2>(Allocator.Temp);
-        for (int j = 0; j < wallIndiciesAroundDest.Length; j++)
+        if (wallIndiciesAroundDest.Length > 0)
         {
-            WallObject wall = WallObjectList[wallIndiciesAroundDest[j]];
-            collidingEdgesDest.Clear();
-            CheckCollisionsForce(wall, dest2d, AgentMovementData[index].Radius, collidingEdgesDest, seperationForcesDest);
-        }
-        if (seperationForcesDest.Length != 0)
-        {
-            for(int i = 0; i < seperationForcesDest.Length; i++)
+            Collision collisionOutput;
+            if (GetCollision(WallObjectList[wallIndiciesAroundDest[0]], dest2d, AgentMovementData[index].Radius, out collisionOutput))
             {
-                sum += seperationForces[i];
+                float2 seperationForce = SolveCollision(collisionOutput, dest2d, AgentMovementData[index].Radius);
+                float2 newDest2d = dest2d + seperationForce;
+                float2 newDirection = math.select(math.normalize(newDest2d - agentPos2d), 0f, agentPos2d.Equals(newDest2d));
+                AgentDirections[index] = newDirection;
             }
-            dest2d += sum;
-            agentPos2d = new float2(transform.position.x, transform.position.z);
-            float2 newDir2d = math.select(math.normalize(dest2d - agentPos2d), 0, dest2d.Equals(agentPos2d));
-            AgentDirections[index] = newDir2d;
         }
     }
 
@@ -186,13 +174,17 @@ public struct CollisionCalculationJob : IJobParallelForTransform
                 if (rhDistance < lhDistance)
                 {
                     isColliding = rhDistance <= agentRadius;
-                    seperationForce = isOutside ? seperationDirection * (agentRadius - math.abs(rhDistance)) : seperationDirection * (agentRadius + math.abs(rhDistance));
+                    float2 cornerSeperationDirection = isOutside ? math.normalize(point - rh) : math.normalize(rh - point);
+                    cornerSeperationDirection = math.select(cornerSeperationDirection, seperationDirection, cornerSeperationDirection.Equals(0));
+                    seperationForce = isOutside ? cornerSeperationDirection * (agentRadius - math.abs(rhDistance)) : cornerSeperationDirection * (agentRadius + math.abs(rhDistance));
                     return isColliding;
                 }
                 else
                 {
                     isColliding = lhDistance <= agentRadius;
-                    seperationForce = isOutside ? seperationDirection * (agentRadius - math.abs(lhDistance)) : seperationDirection * (agentRadius + math.abs(lhDistance));
+                    float2 cornerSeperationDirection = isOutside ? math.normalize(point - lh) : math.normalize(lh - point);
+                    cornerSeperationDirection = math.select(cornerSeperationDirection, seperationDirection, cornerSeperationDirection.Equals(0));
+                    seperationForce = isOutside ? cornerSeperationDirection * (agentRadius - math.abs(lhDistance)) : cornerSeperationDirection * (agentRadius + math.abs(lhDistance));
                     return isColliding;
                 }
             }
@@ -230,4 +222,168 @@ public struct CollisionCalculationJob : IJobParallelForTransform
             return false;
         }
     }
+    bool GetCollision(WallObject wallObj, float2 point, float agentRadius, out Collision collision)
+    {
+        collision = new Collision()
+        {
+            edge = new Edge()
+            {
+                dir = Direction.None,
+                p1 = 0,
+                p2 = 0,
+            },
+            distance = float.MaxValue,
+        };
+        bool isColliding = false;
+
+        NativeSlice<float2> vertexSequence = GetVerteciesOf(wallObj);
+        NativeSlice<Direction> edgeDirections = GetEdgeDirectionsOf(wallObj);
+        for (int i = 0; i < vertexSequence.Length - 1; i++)
+        {
+            Edge curEdge = new Edge()
+            {
+                p1 = vertexSequence[i],
+                p2 = vertexSequence[i + 1],
+                dir = edgeDirections[i],
+            };
+            float colDistance = 0;
+            if (IsColliding(curEdge, out colDistance))
+            {
+                if(colDistance < collision.distance)
+                {
+                    collision.edge = curEdge;
+                    collision.distance = colDistance;
+                }
+                isColliding = true;
+            }
+        }
+        return isColliding;
+
+        bool IsColliding(Edge edge, out float colDistance)
+        {
+            if (edge.p1.y == edge.p2.y) //M = 0
+            {
+                float2 rh = math.select(edge.p2, edge.p1, edge.p1.x > edge.p2.x);
+                float2 lh = math.select(edge.p2, edge.p1, edge.p1.x < edge.p2.x);
+                bool isOutside = (edge.dir == Direction.N && point.y < lh.y) || (edge.dir == Direction.S && point.y > lh.y);
+                float2 seperationDirection = math.select(new float2(0, 1), new float2(0, -1), edge.dir == Direction.N);
+                if (point.x < rh.x && point.x > lh.x)
+                {
+                    float yDistance = math.abs(lh.y - point.y);
+                    colDistance = yDistance;
+                    return yDistance <= agentRadius;
+                }
+                float lhDistance = math.distance(lh, point);
+                float rhDistance = math.distance(rh, point);
+                if (rhDistance < lhDistance)
+                {
+                    colDistance = rhDistance;
+                    return rhDistance <= agentRadius;
+                }
+                else
+                {
+                    colDistance = lhDistance;
+                    return lhDistance <= agentRadius;
+                }
+            }
+            else if (edge.p1.x == edge.p2.x) //M = INFINITY
+            {
+                float2 up = math.select(edge.p2, edge.p1, edge.p1.y > edge.p2.y);
+                float2 down = math.select(edge.p2, edge.p1, edge.p1.y < edge.p2.y);
+                bool isOutside = (edge.dir == Direction.E && point.x < down.x) || (edge.dir == Direction.W && point.x > down.x);
+                float2 seperationDirection = math.select(new float2(1, 0), new float2(-1, 0), edge.dir == Direction.E);
+                if (point.y < up.y && point.y > down.y)
+                {
+                    float xDistance = math.abs(down.x - point.x);
+                    colDistance = xDistance;
+                    return xDistance <= agentRadius;
+                }
+                float upDistance = math.distance(down, point);
+                float downDistance = math.distance(up, point);
+                if (downDistance < upDistance)
+                {
+                    colDistance = downDistance;
+                    return downDistance <= agentRadius;
+                }
+                else
+                {
+                    colDistance = upDistance;
+                    return upDistance <= agentRadius;
+                }
+            }
+            colDistance = float.MaxValue;
+            return false;
+        }
+
+    }
+    float2 SolveCollision(Collision collision, float2 point, float agentRadius)
+    {
+        Edge edge = collision.edge;
+        float2 seperationForce = 0;
+        if (edge.p1.y == edge.p2.y) //M = 0
+        {
+            float2 rh = math.select(edge.p2, edge.p1, edge.p1.x > edge.p2.x);
+            float2 lh = math.select(edge.p2, edge.p1, edge.p1.x < edge.p2.x);
+            bool isOutside = (edge.dir == Direction.N && point.y < lh.y) || (edge.dir == Direction.S && point.y > lh.y);
+            float2 seperationDirection = math.select(new float2(0, 1), new float2(0, -1), edge.dir == Direction.N);
+            if (point.x < rh.x && point.x > lh.x)
+            {
+                float yDistance = lh.y - point.y;
+                seperationForce = isOutside ? seperationDirection * (agentRadius - math.abs(yDistance)) : seperationDirection * (agentRadius + math.abs(yDistance));
+                return seperationForce;
+            }
+            float lhDistance = math.distance(lh, point);
+            float rhDistance = math.distance(rh, point);
+            if (rhDistance < lhDistance)
+            {
+                float2 cornerSeperationDirection = isOutside ? math.normalize(point - rh) : math.normalize(rh - point);
+                cornerSeperationDirection = math.select(cornerSeperationDirection, seperationDirection, cornerSeperationDirection.Equals(0));
+                seperationForce = isOutside ? cornerSeperationDirection * (agentRadius - math.abs(rhDistance)) : cornerSeperationDirection * (agentRadius + math.abs(rhDistance));
+                return seperationForce;
+            }
+            else
+            {
+                float2 cornerSeperationDirection = isOutside ? math.normalize(point - lh) : math.normalize(lh - point);
+                cornerSeperationDirection = math.select(cornerSeperationDirection, seperationDirection, cornerSeperationDirection.Equals(0));
+                seperationForce = isOutside ? cornerSeperationDirection * (agentRadius - math.abs(lhDistance)) : cornerSeperationDirection * (agentRadius + math.abs(lhDistance));
+                return seperationForce;
+            }
+        }
+        else if (edge.p1.x == edge.p2.x) //M = INFINITY
+        {
+            float2 up = math.select(edge.p2, edge.p1, edge.p1.y > edge.p2.y);
+            float2 down = math.select(edge.p2, edge.p1, edge.p1.y < edge.p2.y);
+            bool isOutside = (edge.dir == Direction.E && point.x < down.x) || (edge.dir == Direction.W && point.x > down.x);
+            float2 seperationDirection = math.select(new float2(1, 0), new float2(-1, 0), edge.dir == Direction.E);
+            if (point.y < up.y && point.y > down.y)
+            {
+                float xDistance = down.x - point.x;
+                float2 intersection = point + new float2(xDistance, 0);
+                seperationForce = isOutside ? seperationDirection * (agentRadius - math.abs(xDistance)) : seperationDirection * (agentRadius + math.abs(xDistance));
+                return seperationForce;
+            }
+            float upDistance = math.distance(down, point);
+            float downDistance = math.distance(up, point);
+            if (downDistance < upDistance)
+            {
+                float2 cornerSeperationDirection = isOutside ? math.normalize(point - down) : math.normalize(down - point);
+                cornerSeperationDirection = math.select(cornerSeperationDirection, seperationDirection, cornerSeperationDirection.Equals(0));
+                seperationForce = isOutside ? cornerSeperationDirection * (agentRadius - math.abs(downDistance)) : cornerSeperationDirection * (agentRadius + math.abs(downDistance));
+                return seperationForce;
+            }
+            else
+            {
+                float2 cornerSeperationDirection = isOutside ? math.normalize(point - up) : math.normalize(up - point);
+                cornerSeperationDirection = math.select(cornerSeperationDirection, seperationDirection, cornerSeperationDirection.Equals(0));
+                seperationForce = isOutside ? cornerSeperationDirection * (agentRadius - math.abs(upDistance)) : cornerSeperationDirection * (agentRadius + math.abs(upDistance));
+                return seperationForce;
+            }
+        }
+        return seperationForce;
+    }
+}
+public struct Collision
+{
+    public Edge edge;
+    public float distance;
 }
