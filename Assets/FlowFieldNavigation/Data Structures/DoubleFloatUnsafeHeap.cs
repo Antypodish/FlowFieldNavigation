a6c81231800697422d94e39487e93a9a386fc582
Morterolath <ayberkk0705@gmail.com>
2024-03-02 15:28:25 +0300

    unsafe heaps are moved to their own files.

---
+++ Assets/FlowFieldNavigation/Data Structures/DoubleFloatUnsafeHeap.cs
@@ -0,0 +1,159 @@
+﻿using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Burst;
+namespace FlowFieldNavigation
+{
+    [BurstCompile]
+    internal struct DoubleFloatUnsafeHeap<T> where T : unmanaged
+    {
+        internal UnsafeList<HeapElement<T>> _array;
+        internal T this[int index]
+        {
+            get
+            {
+                return _array[index].data;
+            }
+        }
+        internal bool IsEmpty
+        {
+            get
+            {
+                return _array.IsEmpty;
+            }
+        }
+        internal DoubleFloatUnsafeHeap(int size, Allocator allocator)
+        {
+            _array = new UnsafeList<HeapElement<T>>(size, allocator);
+        }
+        internal void Clear()
+        {
+            _array.Clear();
+        }
+        internal void Add(T element, float pri1, float pri2)
+        {
+            int elementIndex = _array.Length;
+            _array.Add(new HeapElement<T>(element, pri1, pri2));
+            if (elementIndex != 0)
+            {
+                HeapifyUp(elementIndex);
+            }
+        }
+        internal T GetMin() => _array[0].data;
+        internal T ExtractMin()
+        {
+            T min = _array[0].data;
+            HeapElement<T> last = _array[_array.Length - 1];
+            _array[0] = last;
+            _array.Length--;
+            if (_array.Length > 1)
+            {
+                HeapifyDown(0);
+            }
+            return min;
+        }
+        internal void SetPriority(int index, float pri1)
+        {
+            int length = _array.Length;
+            HeapElement<T> cur = _array[index];
+            cur.pri1 = pri1;
+            _array[index] = cur;
+            int parIndex = index / 2 - 1;
+            int lcIndex = index * 2 + 1;
+            int rcIndex = index * 2 + 2;
+            parIndex = math.select(index, parIndex, parIndex >= 0);
+            lcIndex = math.select(index, lcIndex, lcIndex < length);
+            rcIndex = math.select(index, rcIndex, rcIndex < length);
+            HeapElement<T> parent = _array[parIndex];
+            if (cur.pri1 < parent.pri1 || (cur.pri1 == parent.pri1 && cur.pri2 < parent.pri2))
+            {
+                HeapifyUp(index);
+            }
+            else
+            {
+                HeapifyDown(index);
+            }
+        }
+        internal void Dispose()
+        {
+            _array.Dispose();
+        }
+
+        void HeapifyUp(int startIndex)
+        {
+            int curIndex = startIndex;
+            int parIndex = (curIndex - 1) / 2;
+            HeapElement<T> cur = _array[startIndex];
+            HeapElement<T> par = _array[parIndex];
+            bool isCurSmaller = cur.pri1 < par.pri1 || (cur.pri1 == par.pri1 && cur.pri2 < par.pri2);
+            while (isCurSmaller)
+            {
+                _array[parIndex] = cur;
+                _array[curIndex] = par;
+                curIndex = parIndex;
+                parIndex = math.select((curIndex - 1) / 2, 0, curIndex == 0);
+                par = _array[parIndex];
+                isCurSmaller = cur.pri1 < par.pri1 || (cur.pri1 == par.pri1 && cur.pri2 < par.pri2);
+            }
+        }
+        void HeapifyDown(int startIndex)
+        {
+            int length = _array.Length;
+            int curIndex = startIndex;
+            int lcIndex = startIndex * 2 + 1;
+            int rcIndex = lcIndex + 1;
+            lcIndex = math.select(curIndex, lcIndex, lcIndex < length);
+            rcIndex = math.select(curIndex, rcIndex, rcIndex < length);
+            HeapElement<T> cur;
+            HeapElement<T> lc;
+            HeapElement<T> rc;
+            while (lcIndex != curIndex)
+            {
+                cur = _array[curIndex];
+                lc = _array[lcIndex];
+                rc = _array[rcIndex];
+                bool lcSmallerThanRc = lc.pri1 < rc.pri1 || (lc.pri1 == rc.pri1 && lc.pri2 < rc.pri2);
+                bool lcSmallerThanCur = lc.pri1 < cur.pri1 || (lc.pri1 == cur.pri1 && lc.pri2 < cur.pri2);
+                bool rcSmallerThanCur = rc.pri1 < cur.pri1 || (rc.pri1 == cur.pri1 && rc.pri2 < cur.pri2);
+
+                if (lcSmallerThanRc && lcSmallerThanCur)
+                {
+                    _array[curIndex] = lc;
+                    _array[lcIndex] = cur;
+                    curIndex = lcIndex;
+                    lcIndex = curIndex * 2 + 1;
+                    rcIndex = lcIndex + 1;
+                    lcIndex = math.select(lcIndex, curIndex, lcIndex >= length);
+                    rcIndex = math.select(rcIndex, curIndex, rcIndex >= length);
+                }
+                else if (!lcSmallerThanRc && rcSmallerThanCur)
+                {
+                    _array[curIndex] = rc;
+                    _array[rcIndex] = cur;
+                    curIndex = rcIndex;
+                    lcIndex = curIndex * 2 + 1;
+                    rcIndex = lcIndex + 1;
+                    lcIndex = math.select(lcIndex, curIndex, lcIndex >= length);
+                    rcIndex = math.select(rcIndex, curIndex, rcIndex >= length);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        internal struct HeapElement<T> where T : unmanaged
+        {
+            internal T data;
+            internal float pri1;
+            internal float pri2;
+
+            internal HeapElement(T data, float pri1, float pri2)
+            {
+                this.data = data;
+                this.pri1 = pri1;
+                this.pri2 = pri2;
+            }
+        }
+    }
+}
---
+++ Assets/FlowFieldNavigation/Data Structures/SingleFloatUnsafeHeap.cs
@@ -0,0 +1,158 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Mathematics;
+
+namespace FlowFieldNavigation
+{
+    [BurstCompile]
+    internal struct SingleFloatUnsafeHeap<T> where T : unmanaged
+    {
+        internal UnsafeList<HeapElement<T>> _array;
+        internal T this[int index]
+        {
+            get
+            {
+                return _array[index].data;
+            }
+        }
+        internal bool IsEmpty
+        {
+            get
+            {
+                return _array.IsEmpty;
+            }
+        }
+        internal SingleFloatUnsafeHeap(int size, Allocator allocator)
+        {
+            _array = new UnsafeList<HeapElement<T>>(size, allocator);
+        }
+        internal void Clear()
+        {
+            _array.Clear();
+        }
+        internal void Add(T element, float pri)
+        {
+            int elementIndex = _array.Length;
+            _array.Add(new HeapElement<T>(element, pri));
+            if (elementIndex != 0)
+            {
+                HeapifyUp(elementIndex);
+            }
+        }
+        internal T GetMin() => _array[0].data;
+        internal T ExtractMin()
+        {
+            T min = _array[0].data;
+            HeapElement<T> last = _array[_array.Length - 1];
+            _array[0] = last;
+            _array.Length--;
+            if (_array.Length > 1)
+            {
+                HeapifyDown(0);
+            }
+            return min;
+        }
+        internal void SetPriority(int index, float pri)
+        {
+            int length = _array.Length;
+            HeapElement<T> cur = _array[index];
+            cur.pri = pri;
+            _array[index] = cur;
+            int parIndex = index / 2 - 1;
+            int lcIndex = index * 2 + 1;
+            int rcIndex = index * 2 + 2;
+            parIndex = math.select(index, parIndex, parIndex >= 0);
+            lcIndex = math.select(index, lcIndex, lcIndex < length);
+            rcIndex = math.select(index, rcIndex, rcIndex < length);
+            HeapElement<T> parent = _array[parIndex];
+            if (cur.pri < parent.pri)
+            {
+                HeapifyUp(index);
+            }
+            else
+            {
+                HeapifyDown(index);
+            }
+        }
+        internal void Dispose()
+        {
+            _array.Dispose();
+        }
+
+        void HeapifyUp(int startIndex)
+        {
+            int curIndex = startIndex;
+            int parIndex = (curIndex - 1) / 2;
+            HeapElement<T> cur = _array[startIndex];
+            HeapElement<T> par = _array[parIndex];
+            bool isCurSmaller = cur.pri < par.pri;
+            while (isCurSmaller)
+            {
+                _array[parIndex] = cur;
+                _array[curIndex] = par;
+                curIndex = parIndex;
+                parIndex = math.select((curIndex - 1) / 2, 0, curIndex == 0);
+                par = _array[parIndex];
+                isCurSmaller = cur.pri < par.pri;
+            }
+        }
+        void HeapifyDown(int startIndex)
+        {
+            int length = _array.Length;
+            int curIndex = startIndex;
+            int lcIndex = startIndex * 2 + 1;
+            int rcIndex = lcIndex + 1;
+            lcIndex = math.select(curIndex, lcIndex, lcIndex < length);
+            rcIndex = math.select(curIndex, rcIndex, rcIndex < length);
+            HeapElement<T> cur;
+            HeapElement<T> lc;
+            HeapElement<T> rc;
+            while (lcIndex != curIndex)
+            {
+                cur = _array[curIndex];
+                lc = _array[lcIndex];
+                rc = _array[rcIndex];
+                bool lcSmallerThanRc = lc.pri < rc.pri;
+                bool lcSmallerThanCur = lc.pri < cur.pri;
+                bool rcSmallerThanCur = rc.pri < cur.pri;
+
+                if (lcSmallerThanRc && lcSmallerThanCur)
+                {
+                    _array[curIndex] = lc;
+                    _array[lcIndex] = cur;
+                    curIndex = lcIndex;
+                    lcIndex = curIndex * 2 + 1;
+                    rcIndex = lcIndex + 1;
+                    lcIndex = math.select(lcIndex, curIndex, lcIndex >= length);
+                    rcIndex = math.select(rcIndex, curIndex, rcIndex >= length);
+                }
+                else if (!lcSmallerThanRc && rcSmallerThanCur)
+                {
+                    _array[curIndex] = rc;
+                    _array[rcIndex] = cur;
+                    curIndex = rcIndex;
+                    lcIndex = curIndex * 2 + 1;
+                    rcIndex = lcIndex + 1;
+                    lcIndex = math.select(lcIndex, curIndex, lcIndex >= length);
+                    rcIndex = math.select(rcIndex, curIndex, rcIndex >= length);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        internal struct HeapElement<T> where T : unmanaged
+        {
+            internal T data;
+            internal float pri;
+
+            internal HeapElement(T data, float pri)
+            {
+                this.data = data;
+                this.pri = pri;
+            }
+        }
+    }
+}
